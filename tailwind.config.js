32442bbb549ce21dbc9a0f3f06683b6b3afd375e
Sainos <qa.kadasoftware@gmail.com>
2025-06-10 02:43:03 -0600

    feat: update Angular dependencies and enhance dashboard UI - Se añadió la dependencia @angular/animations a package.json. - Se refactorizó AppComponent para eliminar la lógica de autenticación y simplificar la plantilla. - Se actualizó el enrutamiento para incluir una ruta de registro. - Se mejoró el componente del panel con información del usuario y un diseño mejorado. - Se implementó la función de cierre de sesión en el panel. - Se rediseñó el componente de inicio de sesión con un nuevo diseño y validación de formulario. - Se creó un nuevo componente de registro con validación de formulario y confirmación de contraseña. - Se añadió la integración con Flowbite para mejorar el estilo y los componentes. - Se actualizó la configuración de CSS de Tailwind para incluir Flowbite. - Se añadieron estilos CSS para los componentes de inicio de sesión y registro..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
  /** @type {import('tailwindcss').Config} */
  export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
