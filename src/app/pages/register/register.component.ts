22ad7789fa3410a13e084f5ca79a42e336bae488
Niky11 <nancyobed11@gmail.com>
2025-06-10 22:22:18 -0600

    actualizando el register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router,RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,RouterModule,CommonModule],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
